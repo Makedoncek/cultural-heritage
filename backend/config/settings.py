447a86f0e8b3b3aab88ff03475fd33f7b206d8ef
Makedoncek <123456v64@gmail.com>
2026-02-27 15:52:54 +0100

    Clean up deployment prep: remove unused imports, fix indentation

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import os
-from email.policy import default
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
@@ -190,7 +188,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
